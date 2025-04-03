24fd715c50d8694462620d0ba16f5957389e6bbd
jonvds <jonva216@gmail.com>
2025-04-03 16:55:57 +0200

    Metido SteamApi en DeltaLake

---
+++ src/Steam/DeltaWriterUsers.py
@@ -0,0 +1,51 @@
+import os
+from pyspark.sql import SparkSession
+from delta import configure_spark_with_delta_pip
+from pyspark.sql.types import (
+    StructType, StructField, StringType, LongType, DoubleType, ArrayType, MapType
+)
+
+class DeltaWriter:
+    def __init__(self, delta_path="file:///data/delta/Steam_user_data", master="local[*]"):
+        self.delta_path = delta_path
+        self.spark = self._create_spark_session(master)
+        self.schema = StructType([
+            StructField("SteamID", StringType(), True),
+            StructField("Username", StringType(), True),
+            StructField("Country", StringType(), True),
+            StructField("Steam_Level", LongType(), True),
+            StructField("Owned_Games", ArrayType(MapType(StringType(), StringType())), True),
+            StructField("Profile_URL", StringType(), True),
+        ])
+
+    def _create_spark_session(self, master):
+        builder = (
+            SparkSession.builder
+            .appName("DeltaLakeWriter")
+            .master(master)
+            .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+            .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+        )
+
+        return configure_spark_with_delta_pip(builder).getOrCreate()
+
+    def write_records(self, records):
+        if not records:
+            print("No records to write.")
+            return
+
+        for record in records:
+            if not isinstance(record.get("Owned_Games"), list):
+                record["Owned_Games"] = []
+            else:
+                record["Owned_Games"] = [
+                    {str(k): str(v) for k, v in game.items()}
+                    for game in record["Owned_Games"]
+                ]
+
+        df = self.spark.createDataFrame(records, schema=self.schema)
+        df.write.format("delta").mode("append").save(self.delta_path)
+
+   
+
+        print(f"Wrote {len(records)} records to Delta Lake.")
---
+++ src/Steam/SteamDataLoader.py
@@ -0,0 +1,37 @@
+import logging
+#from dotenv import load_dotenv
+from SteamGamesAPI import fetch_steam_game_data , fetch_steam_game_data_json
+from SteamUsersAPI import fetch_steam_user_data , fetch_steam_user_data_json
+from DeltaWriterGames import DeltaWriter as DG
+from DeltaWriterUsers import DeltaWriter as DU
+
+# Load environment variables
+#load_dotenv()
+
+# Setup logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Initialize Delta writers
+delta_writer_games = DG()  
+delta_writer_users = DU()
+
+# Function to fetch and store data
+def fetch_and_store_steam_data():
+    logging.info('Fetching Steam game data...')
+    records = fetch_steam_game_data_json() #Change this to call API instead of json
+    if records:
+        logging.info(f"Writing {len(records)} Game records to Delta Lake...")
+        delta_writer_games.write_records(records)
+    else:
+        logging.info("No Game data to write.")
+
+    records= fetch_steam_user_data_json() #Change this to call API instead of json
+    if records:
+        logging.info(f"Writing {len(records)} User records to Delta Lake...")
+        delta_writer_users.write_records(records)
+    else:
+        logging.info("No User data to write.")
+
+if __name__ == '__main__':
+    logging.info(f'Starting one-time Steam data collection...')
+    fetch_and_store_steam_data()
